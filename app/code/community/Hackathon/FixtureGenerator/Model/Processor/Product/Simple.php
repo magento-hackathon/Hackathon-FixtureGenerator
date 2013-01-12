9ba0561b89a31d9cda3b0a09c787e01b9bd76ca1
Rouven Alexander Rieker <therouv@googlemail.com>
2013-01-12 21:19:41 +0100

    Added populate logic if an attribute does not exists / ATTENTION: Build is failing due to missing generator api

--- app/code/community/Hackathon/FixtureGenerator/Model/Processor/Product/Simple.php
+++ app/code/community/Hackathon/FixtureGenerator/Model/Processor/Product/Simple.php
@@ -23,17 +23,54 @@ class Hackathon_FixtureGenerator_Model_Processor_Product_Simple
     public function process(array $data)
     {
         $numberOfIterations = (isset($data['number'])) ? $data['number'] : 1;
+        unset($data['number']);
 
+
+        $requiredAttributes = $this->getRequiredAttributes();
         $products = array();
         for ($i = 1; $i <= $numberOfIterations; $i++) {
-            $data = array(
-                'entity_id' => $i,
-                'type_id' => 'simple',
-                'sku' => 'test_'.$i,
-                'name' => 'Test '.$i
-            );
-            $products[] = $data;
+            $productData = array();
+            foreach ($data as $key => $value) {
+                $productData[$key] = $this->getProcessedValue($value);
+            }
+
+            foreach ($requiredAttributes as $attribute) {
+                if (!isset($productData[$attribute])) {
+                    $defaultValue = $this->getDefaultValue($attribute);
+                    $productData[$attribute] = $this->getProcessedValue($defaultValue);
+                }
+            }
+
+            $products[] = $productData;
         }
         return $products;
+    }
+
+    public function getProcessedValue($string)
+    {
+        return $string;
+    }
+
+    public function getRequiredAttributes()
+    {
+        $attributes = array(
+            'entity_id',
+            'type_id',
+            'description',
+            'weight',
+            'price',
+            'tax_class_id',
+            'status',
+            'visibility',
+            'description',
+            'short_description'
+        );
+        return $attributes;
+    }
+
+    public function getDefaultValue($attribute)
+    {
+        $node = 'phpunit/testdata/processor/product/simple/'.$attribute;
+        return (string) Mage::getConfig()->getNode($node);
     }
 }
