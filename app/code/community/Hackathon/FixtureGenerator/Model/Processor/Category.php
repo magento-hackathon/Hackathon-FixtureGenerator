ec0f66ce2a90f9a7fae080a3f41626fce49ea903
Dima Janzen <dima.janzen@gmail.com>
2013-01-13 10:38:02 +0100

    added Category processor with failing test

---
+++ app/code/community/Hackathon/FixtureGenerator/Model/Processor/Category.php
@@ -0,0 +1,8 @@
+<?php
+class Hackathon_FixtureGenerator_Model_Processor_Category implements Hackathon_FixtureGenerator_Model_Processor_Interface
+{
+    public function process(array $data)
+    {
+        return array();
+    }
+}
