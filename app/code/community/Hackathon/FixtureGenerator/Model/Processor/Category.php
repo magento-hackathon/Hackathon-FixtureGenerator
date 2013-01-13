45e8cbb6c2462f8790f416c75068fbea678e9d70
Dima Janzen <dima.janzen@gmail.com>
2013-01-13 11:15:49 +0100

    fixed failing test

--- app/code/community/Hackathon/FixtureGenerator/Model/Processor/Category.php
+++ app/code/community/Hackathon/FixtureGenerator/Model/Processor/Category.php
@@ -1,8 +1,36 @@
 <?php
-class Hackathon_FixtureGenerator_Model_Processor_Category implements Hackathon_FixtureGenerator_Model_Processor_Interface
+class Hackathon_FixtureGenerator_Model_Processor_Category extends  Hackathon_FixtureGenerator_Model_Processor_Abstract
 {
+  protected $requiredKeys = array(
+      'entity_id',
+      'name'
+    );
+
+    /**
+     * Process the given data
+     *
+     * @param array $data
+     * @return mixed
+     */
     public function process(array $data)
     {
-        return array();
+        $numberOfIterations = 1;
+
+        if (isset($data['number'])){
+            $numberOfIterations = $data['number'];
+            unset($data['number']);
+        }
+
+        $data = array_merge($this->getDefaultData(), $data);
+
+        $this->initialize($data);
+
+        $categories = array();
+        for ($i = 1; $i <= $numberOfIterations; $i++) {
+            $categoryData = $this->generate($data);
+            $categories[] = $categoryData;
+        }
+
+        return $categories;
     }
 }
