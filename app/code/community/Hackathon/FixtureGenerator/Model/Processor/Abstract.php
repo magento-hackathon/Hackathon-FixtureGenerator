df9566e06ac14237f0d79c10a15c4216e186a3a3
Rouven Alexander Rieker <therouv@googlemail.com>
2013-01-13 12:11:46 +0100

    Added class/function comments and code cleanup

--- app/code/community/Hackathon/FixtureGenerator/Model/Processor/Abstract.php
+++ app/code/community/Hackathon/FixtureGenerator/Model/Processor/Abstract.php
@@ -1,18 +1,43 @@
 <?php
-
+/**
+ * Fixture Generator module for PHP Unit test suite for Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Hackathon
+ * @package    Hackathon_FixtureGenerator
+ * @author     Peter Jaap Blaakmeer <https://github.com/peterjaap>
+ * @author     Ivan Chepurnyi <https://github.com/IvanChepurnyi>
+ * @author     Dima Janzen <https://github.com/dimajanzen>
+ * @author     Rouven Alexander Rieker <https://github.com/therouv>
+ * @author     Michael Ryvlin <https://github.com/mryvlin>
+ * @copyright  2013 Hackathon Dev Team (http://www.magento-hackathon.de/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link       https://github.com/magento-hackathon/Hackathon-FixtureGenerator/
+ */
 abstract class Hackathon_FixtureGenerator_Model_Processor_Abstract
     implements Hackathon_FixtureGenerator_Model_Processor_Interface
 {
     const XML_PATH_TESTDATA_PROCESSOR = 'phpunit/testdata/processor/%s';
 
+    /**
+     * @var string
+     */
     protected $type;
 
     /**
-     *
      * @var Hackathon_FixtureGenerator_Model_Generator_Container[]
      */
     protected $generators = array();
 
+    /**
+     * @var array
+     */
     protected $requiredKeys = array();
 
     /**
